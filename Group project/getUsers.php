e0a95730864067757fc98c70a9f98336dc634b62
linkvetern64 <jstand1@umbc.edu>
2016-05-01 01:51:37 -0400

    Working on suggested pins

--- Group project/getUsers.php
+++ Group project/getUsers.php
@@ -6,10 +6,47 @@
  * Time: 11:54 PM
  */
 require_once ("libs.php");
+$dataentered = $_POST["sq"];
 $conn = connect();
-$query = "SELECT pin FROM user_login WHERE 1";
-$result = mysql_fetch_array(mysql_query($query));
+$result = mysql_query("SELECT pin FROM user_login");
 
-foreach($result as $val){
-    echo($val . "<br>");
+while($row = mysql_fetch_array($result)){
+    $a[] = "$row[0]";
 }
+
+//lookup all hints from array if length of q>0
+if (strlen($dataentered) > 0)
+{
+  $hint="";
+  
+  for($i=0; $i<count($a); $i++)
+  {
+  if (strtolower($dataentered)==strtolower(substr($a[$i],0,strlen($dataentered))))
+    {
+    if ($hint=="")
+      {
+         $hint=$a[$i];
+		 
+      }
+	  else
+      {
+		$hint=$hint." , ".$a[$i];
+      }
+    }
+  }
+  
+  $choices = explode(",", $hint);
+  echo "<ul>";
+  $i = 0;
+  foreach ($choices as $option){
+	if($i < 3){
+		echo "<li>$option</li><br/>";
+  		$i++;
+  	}
+  }
+  echo "</ul>";
+}	
+	
+	
+
+disconnect($conn);
