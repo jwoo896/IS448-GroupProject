1d1d61d630305e719a3466f31dc165e6c8200a08
Jae Whan Woo <jwoo896@gmail.com>
2016-06-09 20:16:07 -0400

    changed names

--- Group project/updateInventory.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once("libs.php");
-	$prodId = $_GET['item'];
-	$conn = connect();
-	$sql = "UPDATE inventory SET requested=1 WHERE product_id='$prodId'";
-	$result = mysql_query($sql);
-	disconnect($conn);
-	echo "$prodId";
-?>
