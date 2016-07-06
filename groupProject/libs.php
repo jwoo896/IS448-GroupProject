9bc706c3fcde52f2a4e195432847cde559f51020
Joseph Woo <jwoo@Josephs-Air.home>
2016-07-06 17:58:49 -0400

    updated libs file

--- groupProject/libs.php
+++ groupProject/libs.php
@@ -8,12 +8,12 @@ session_start();
 function connect(){
 
 	$db ="jstand1";
-	$conn = mysql_connect("studentdb-maria.gl.umbc.edu", "jstand1", "jstand1");
+	$conn = mysql_connect("192.168.1.94/phpmyadmin", "root", "1Asxdcfv");
 	if(!$conn){
 		echo("Could not connect to MySQL");
 	}
-
-	$er = mysql_select_db("jstand1");
+	//finish phpmyadmin config below
+	$er = mysql_select_db("jWoo");
 	if(!$er){
 		echo("Could not find table name");
 	}
