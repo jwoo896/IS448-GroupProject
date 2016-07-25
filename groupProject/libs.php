1853fc651092b67c70246e74595148909e1b67a7
Joseph Woo <jwoo@Josephs-Air.home>
2016-07-25 13:19:36 -0400

    changes

--- groupProject/libs.php
+++ groupProject/libs.php
@@ -8,12 +8,12 @@ session_start();
 function connect(){
 
 	$db ="mysql";
-	$conn = mysql_connect("71.244.148.17/phpmyadmin", "root", "1Asxdcfv");
+	$conn = mysqli_connect("71.244.148.17/phpmyadmin", "root", "1Asxdcfv");
 	if(!$conn){
 		echo("Could not connect to MySQL");
 	}
 	//finish phpmyadmin config below
-	$er = mysql_select_db("user_login");
+	$er = mysql_select_db("mysql");
 	if(!$er){
 		echo("Could not find table name");
 	}
